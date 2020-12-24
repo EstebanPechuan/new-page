5c2037451a88a528ebda55d57987e38eeae07be5
EstebanPechuan <51751504+EstebanPechuan@users.noreply.github.com>
2020-12-24 16:37:21 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,10 @@
+function iconPress() {
+    var menu = document.querySelector('.menu')
+    menu.setAttribute('class', 'menu2')
+    
+}
+
+function iconPress2() {
+    var menu2 = document.querySelector('.menu2')
+    menu2.setAttribute('class', 'menu')
+}
